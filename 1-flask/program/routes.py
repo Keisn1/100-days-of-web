066fc47c08cf3f9e05307ae6abf7cde560be4e16
KayPro <kaypro@Kays-Mac-mini.local>
2022-11-16 16:23:48 +0100

    added htmls

--- 1-flask/program/routes.py
+++ 1-flask/program/routes.py
@@ -1,6 +1,7 @@
 from program import app
+from flask import render_template
 
 @app.route('/')                 # root- or index- path
 @app.route('/index')            # "What is the decorator run against?"
 def index():                # try and name functions similar to url, route-name
-    return 'Hello Pyhonistas'
+    return render_template('index.html')
