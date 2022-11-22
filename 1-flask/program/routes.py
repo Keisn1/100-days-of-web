3cca4d4b9e3fba00767c47602dc70006a4fa620f
KayPro <kaypro@Kays-Mac-mini.local>
2022-11-22 16:25:37 +0100

    end day 7

--- 1-flask/program/routes.py
+++ 1-flask/program/routes.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route('/index')            # "What is the decorator run against?"
 def index():                # try and name functions similar to url, route-name
     return render_template('index.html')
+
+@app.route('/100Days')
+def p100Days():
+    return render_template('100Days.html')
+
