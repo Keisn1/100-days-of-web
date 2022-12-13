b501fccb359ef21330714a51b7f6d69e1e6156f0
KayProMac <kaypromac@Kays-MacBook-Pro.local>
2022-12-13 14:20:25 +0100

    add chapter4 app and test_app

--- chapter4/app.py
+++ chapter4/app.py
@@ -4,16 +4,6 @@ import json
 from apistar import App, Route, types, validators
 from apistar.http import JSONResponse
 
-class Airport(types.Type):
-    id = validators.Integer(allow_null=True)
-    airport_name = validators.String(max_length=100)
-    continent_code = validators.String(max_length=2)
-    country_code = validators.String(max_length=2)
-    municipality = validators.String(max_length=100)
-    gps_code = validators.String(max_length=4)
-    elevation = validators.Integer(minimum=-2000, maximum=20000)
-
-
 def _load_airport_data() -> Dict:
     with open("airport_data.json") as file_:
         airport_data = json.load(file_)
@@ -28,7 +18,8 @@ def _load_airport_data() -> Dict:
             if not ap["gps_code"]:
                 continue
 
-            airports[ap["id"]] = Airport(ap)
+            ap["id"] = len(airports) + 1
+            airports[ap["id"]] = ap
     return airports
 
 airports = _load_airport_data()
@@ -37,6 +28,17 @@ VALID_COUNTRY_CODES = set([ap["country_code"] for ap in airports.values()])
 all_elevation = set([ap["elevation"] for ap in airports.values()])
 AIRPORT_NOT_FOUND = "Airport not found"
 AIRPORT_ALREADY_EXISTS = "Airport is already in the database"
+
+
+class Airport(types.Type):
+    id = validators.Integer(allow_null=True)
+    airport_name = validators.String(max_length=100)
+    continent_code = validators.String(enum=list(VALID_CONTINENT_CODES))
+    country_code = validators.String(enum=list(VALID_COUNTRY_CODES))
+    municipality = validators.String(max_length=100)
+    gps_code = validators.String(max_length=4)
+    elevation = validators.Integer(minimum=-2000, maximum=20000)
+
 
 def list_all_airports() -> List[Airport]:
     return [Airport(ap) for ap in airports.values()]
@@ -50,23 +52,15 @@ def get_airport(ap_id: int) -> JSONResponse:
     return JSONResponse(ap, status_code=200)
 
 def create_airport(ap: Airport):
-    id_ = len(airports.keys()) + 1000
-    ap_names = set([ap.airport_name for ap in airports.values()])
+    # ap.id = len(airports.keys()) + 1 this would not work once you add and delete airports - setting new ap to an id always present
+    ap.id = max(airports.keys()) + 1
+    ap_names = set([ap["airport_name"] for ap in airports.values()])
     if ap.airport_name in ap_names:
         error = {'error': AIRPORT_ALREADY_EXISTS}
         return JSONResponse(error, status_code=409)
 
-    new_ap = {
-        "id": id_,
-        "airport_name": ap.airport_name,
-        "continent_code": ap.continent_code,
-        "country_code": ap.country_code,
-        "municipality": ap.municipality,
-        "gps_code": ap.gps_code,
-        "elevation": ap.elevation
-    }
-    airports[id_] = Airport(new_ap)
-    return JSONResponse(Airport(new_ap), status_code=201)
+    airports[ap.id] = Airport(ap)
+    return JSONResponse(Airport(ap), status_code=201)
 
 def update_airport(ap_id: int, ap: Airport):
     if ap_id not in airports.keys():
@@ -83,7 +77,7 @@ def delete_airport(ap_id: int):
         error = {'error': AIRPORT_NOT_FOUND}
         return JSONResponse(error, status_code=404)
     del airports[ap_id]
-    return JSONResponse({}, status_code=200)
+    return JSONResponse({}, status_code=204)
 
 routes = [
     Route('/', method="GET", handler=list_all_airports),
--- chapter4/test_app.py
+++ chapter4/test_app.py
@@ -6,11 +6,25 @@ client = test.TestClient(app)
 
 @pytest.fixture
 def correct_response_fixture():
-    return {
-        "id": 1, "airport_name": "Imperial Municipal Airport",
-        "continent_code": "NA", "country_code": "US", "municipality": "Imperial",
-        "gps_code": "KIML", "elevation": 3275
-    }
+    return {"id": 1,
+            "airport_name": "Imperial Municipal Airport",
+            "continent_code": "NA",
+            "country_code": "US",
+            "municipality": "Imperial",
+            "gps_code": "KIML",
+            "elevation": 3275
+            }
+
+@pytest.fixture
+def create_airport_fixture():
+    return {'id': '123',
+            'airport_name': 'Flughafen Innsbruck',
+            'continent_code': 'EU',
+            'country_code': 'AT',
+            'municipality': 'Tirol',
+            'gps_code': 'INNS',
+            'elevation': 5000
+            }
 
 @pytest.fixture
 def airport_not_found_fixture():
@@ -40,4 +54,67 @@ def test_get_airport(id, expected_response, request):
         assert response.status_code == 404
         assert response.json() == request.getfixturevalue(expected_response)
 
+@pytest.mark.parametrize('test_input', [
+    ('create_airport_fixture')
+])
+def test_create_airport(test_input, request):
+    num_aps = len(airports)
+    new_ap = request.getfixturevalue(test_input)
+    response = client.post('/', data=new_ap)
+    assert response.status_code == 201
+    new_ap["id"] = num_aps + 1
+    assert response.json() == new_ap
+    assert len(airports) == num_aps + 1
+
+    response = client.get(f'/{num_aps + 1}')
+    assert response.status_code == 200
+    assert response.json() == new_ap
+
+@pytest.mark.parametrize('test_input', [
+    ('correct_response_fixture')
+])
+def test_create_airport_after_delete(test_input, request):
+    correct_airport = request.getfixturevalue(test_input)
+    id = correct_airport["id"]
+    ap_count = len(airports)
+    response = client.delete(f'/{id}')
+    assert response.status_code == 204
+    assert len(airports) == ap_count - 1
+
+    response = client.post('/', data=correct_airport)
+    assert response.status_code == 201
+    assert len(airports) == ap_count
+
+def test_create_airport_missing_fields():
+    data = {'key': 1}
+    response = client.post('/', data=data)
+    assert response.status_code == 400
+
+    errors = response.json()
+    assert errors['airport_name'] == 'The "airport_name" field is required.'
+    assert errors['continent_code'] == 'The "continent_code" field is required.'
+    assert errors['country_code'] == 'The "country_code" field is required.'
+    assert errors['municipality'] == 'The "municipality" field is required.'
+    assert errors['gps_code'] == 'The "gps_code" field is required.'
+    assert errors['elevation'] == 'The "elevation" field is required.'
+
+def test_create_car_validation():
+    data = {
+        "continent_code": "UN",
+        "country_code": "QQ",
+        "elevation": -10000
+    }
+    response = client.post('/', data=data)
+    assert response.status_code == 400
+
+    errors = response.json()
+    assert "Must be one of" in errors['continent_code']
+    assert "Must be one of" in errors['country_code']
+    assert errors['elevation'] == 'Must be greater than or equal to -2000.'
+
+
+def test_airport_not_found():
+    response = client.get('/11111')
+    assert response.status_code == 404
+    assert response.json() == {'error': AIRPORT_NOT_FOUND}
 
